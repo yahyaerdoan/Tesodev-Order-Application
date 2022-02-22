e86e5f2ee5994c85be4161f68a641c8bff52ed2e
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-22 15:56:31 +0300

    Response models structure created.

---
+++ TesodevOrder.CoreLayer/Abstract/IResponses/IResults/IDataResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesodevOrder.CoreLayer.Abstract.IResponses.IResult;
+
+namespace TesodevOrder.CoreLayer.Abstract.IResponses.IResults
+{
+    public interface IDataResult<T> : IResult.IResult
+    {
+        T Data { get; }
+    }
+}

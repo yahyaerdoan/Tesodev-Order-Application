8da5faf6f040ddb730c44d27e2e3c6511e4e6760
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-22 19:29:37 +0300

    Dto models mapped.

---
+++ TesodevOrder.EntityLayer/Concrete/DtoModelMappers/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using TesodevOrder.EntityLayer.Concrete.DtoModels;
+using TesodevOrder.EntityLayer.Concrete.Models;
+
+namespace TesodevOrder.EntityLayer.Concrete.DtoModelMappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Address, DtoAddress>().ReverseMap();
+            CreateMap<Customer, DtoCustomer>().ReverseMap();
+            CreateMap<Order, DtoOrder>().ReverseMap();
+            CreateMap<Product, DtoProduct>().ReverseMap();
+        }
+    }
+}

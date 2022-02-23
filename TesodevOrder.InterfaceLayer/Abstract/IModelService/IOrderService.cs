e8f6a470aae4e81df8f295754127890481468c3c
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-23 23:43:20 +0300

    Service models are written.

---
+++ TesodevOrder.InterfaceLayer/Abstract/IModelService/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesodevOrder.EntityLayer.Concrete.DtoModels;
+using TesodevOrder.EntityLayer.Concrete.Models;
+using TesodevOrder.InterfaceLayer.Abstract.IGenericService;
+
+namespace TesodevOrder.InterfaceLayer.Abstract.IModelService
+{
+    public interface IOrderService : IGenericService<Order, DtoOrder>
+    {
+    }
+}

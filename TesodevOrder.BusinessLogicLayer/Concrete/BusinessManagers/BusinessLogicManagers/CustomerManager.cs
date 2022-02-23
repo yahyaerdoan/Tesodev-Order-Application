655949d4e984e118e75090aabf551375a768c376
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-24 00:52:47 +0300

    Manager class models are written.

--- TesodevOrder.BusinessLogicLayer/Concrete/BusinessManagers/BusinessLogicManagers/CustomerManager.cs
+++ TesodevOrder.BusinessLogicLayer/Concrete/BusinessManagers/BusinessLogicManagers/CustomerManager.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TesodevOrder.BusinesLogicLayer.Concrete.BusinessManagers.GenericBusinessLogics;
+using TesodevOrder.EntityLayer.Concrete.DtoModels;
+using TesodevOrder.EntityLayer.Concrete.Models;
+using TesodevOrder.InterfaceLayer.Abstract.IModelService;
 
 namespace TesodevOrder.BusinessLogicLayer.Concrete.BusinessManagers.BusinessLogicManagers
 {
-   public class CustomerManager
+    public class CustomerManager : GenericBusinessLogicManager<Customer, DtoCustomer>,ICustomerService
     {
     }
 }

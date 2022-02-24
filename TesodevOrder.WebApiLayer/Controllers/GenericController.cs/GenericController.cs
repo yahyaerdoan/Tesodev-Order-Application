7e77acb56a283cf2e314a6adf711f59c0b02a11d
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-24 22:48:46 +0300

    Controller generic structure created.

---
+++ TesodevOrder.WebApiLayer/Controllers/GenericController.cs/GenericController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TesodevOrder.CoreLayer.Concrete.Entities;
+using TesodevOrder.InterfaceLayer.Abstract.IGenericService;
+
+namespace TesodevOrder.WebApiLayer.Controllers.GenericController.cs
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class GenericController<TInterface, T, TDto> : ControllerBase where TInterface : IGenericService<T, TDto> where T : Entity where TDto : Dto
+    {
+        private readonly TInterface _service;
+        public GenericController(TInterface service)
+        {
+            _service = service;
+        }
+    }
+}
